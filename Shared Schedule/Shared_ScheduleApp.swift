511f666c8c61011acd5aa7cc817bc248bb608e60
Weber <kyoii377@gmail.com>
2026-03-01 13:47:20 +0800

    Initial commit

---
+++ Shared Schedule/Shared_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shared_ScheduleApp.swift
+//  Shared Schedule
+//
+//  Created by Wei Chu  on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct Shared_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared ScheduleTests/Shared_ScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shared_ScheduleTests.swift
+//  Shared ScheduleTests
+//
+//  Created by Wei Chu  on 2026/3/1.
+//
+
+import Testing
+@testable import Shared_Schedule
+
+struct Shared_ScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
